ce4345f54e4cadd773fac79559db285d6a7ad21b
Trevor Howard <trevorhoward2@hotmail.com>
2021-10-03 16:20:09 -0600

    Allow non ssl queries to heroku

--- db/db.js
+++ db/db.js
@@ -1,14 +1,15 @@
-const { Pool } = require('pg');
+const pg = require('pg');
 const connectionString = process.env.DATABASE_URL
 
 var pool;
 
 if (process.env.DATABASE_URL) {
-    pool = new Pool({
+    pg.defaults.ssl = { rejectUnauthorized: false }
+    pool = new pg.Pool({
         connectionString,
     })
 } else {
-    pool = new Pool({
+    pool = new pg.Pool({
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
