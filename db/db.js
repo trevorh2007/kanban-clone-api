161f27fda5511fdef1a6fe706a2c3bc33e2ad1b9
Trevor Howard <trevorhoward2@hotmail.com>
2021-10-03 14:47:08 -0600

    Heroku postgres use correct db url for prod

--- db/db.js
+++ db/db.js
@@ -1,11 +1,18 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: 5432
-})
+if (process.env.DATABASE_URL) {
+    const pool = new Pool({
+        connection: process.env.DATABASE_URL
+    })
+} else {
+    const pool = new Pool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: 5432
+    })
+}
+
 
 module.exports = pool;
